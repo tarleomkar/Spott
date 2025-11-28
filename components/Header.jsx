c4b656f2f9960b9517967aa534346d464309033e
Omkar Tarle <tarleomkar8@gmail.com>
2025-11-28 13:26:32 +0530

    Stroing user details in DB and adding loader to the Header side

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,8 +6,14 @@ import Link from "next/link"
 import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import { Button } from './ui/button';
 import { Authenticated, Unauthenticated } from  "convex/react"
+import { BarLoader } from "react-spinners"
+import { useStoreUser } from '@/hooks/use-store-user';
 
 const Header = () => {
+
+  const { isLoading } = useStoreUser()
+
+  const { isLoaded } = useUser();
   return (
     <>
         <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
@@ -43,6 +49,12 @@ const Header = () => {
         </div>
 
             {/* Mobile Search and Location - Below Header */}
+
+            {/* Loader */}
+            {isLoading && (<div className='absolute bottom-0 left-0 w-full'>
+              {/* Loader */}
+              <BarLoader width={"100%"} loading={!isLoaded} color="#a855f7" />
+            </div>)}
         </nav>
 
             {/* Modals */}
